704d362427c5f6e1b3f414ce880bbe514d72e761
Paul Jones <puj@Pauls-MacBook-Air.local>
2018-10-17 17:11:16 +0100

    completed section 8 taylor swift app

--- paul_udemy_1/albums/index.js
+++ paul_udemy_1/albums/index.js
@@ -2,13 +2,19 @@
 /** @format */
 // Import a library to create a new Component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry, View } from 'react-native';
 
-import Header from './src/components/Header'
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// http://rallycoding.herokuapp.com/api/music_albums
 
 // Create a component
 const App = () =>  (
-    <Header />
+    <View style={{ flex: 1 }}>
+        <Header headerText={'Albums!'} />
+        <AlbumList />
+    </View>
 );
 // Render a component
 
